acd6b6983aa8034b135eb9d7961c764925cfdccf
Soma-Kawamoto <ctwn0334@mail4.doshisha.ac.jp>
2026-04-15 21:37:06 +0900

    commit

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,29 @@
+import sqlite3
+from fastapi import FastAPI, Form
+from fastapi.middleware.cors import CORSMiddleware
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["*"],
+    allow_methods=["*"]
+    )
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/save")
+def save_text(text: str = Form(...)):
+    conn = sqlite3.connect("local.db")
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO texts (content) VALUES (?)", (text,))
+
+    conn.commit()
+    conn.close()
+
+    return {"status": "ok", "saved": text}    
